338789d2d8cdd5bf9fb5f6c76a3eee0c1de38b3b
brice <b@bnema.dev>
2026-03-15 21:14:20 +0100

    feat: add server commands (start, init)
    Adds `server start` (opens bbolt store, loads auth key, serves gRPC)
    and `server init` (exports GPG public key, stores in auth bucket).
    Both commands use zerowrap logging and exit with a clear message if
    no GPG key has been registered yet.

---
+++ internal/adapters/cli/server/start.go
@@ -0,0 +1,58 @@
+package server
+
+import (
+	"context"
+	"errors"
+	"os"
+
+	grpcadapter "github.com/bnema/sekeve/internal/adapters/grpc"
+	"github.com/bnema/sekeve/internal/adapters/storage"
+	"github.com/bnema/sekeve/internal/domain/port"
+	"github.com/bnema/zerowrap"
+	"github.com/spf13/cobra"
+)
+
+func NewStartCmd() *cobra.Command {
+	var addr string
+	var dataPath string
+
+	cmd := &cobra.Command{
+		Use:   "start",
+		Short: "Start the sekeve gRPC server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctx := cmd.Context()
+			log := zerowrap.FromCtx(ctx)
+
+			store, err := storage.NewBboltStore(ctx, dataPath)
+			if err != nil {
+				log.Error().Err(err).Msg("failed to open storage")
+				os.Exit(1)
+			}
+			defer store.Close(context.Background())
+
+			pubKey, err := store.GetAuthKey(ctx)
+			if err != nil {
+				if errors.Is(err, port.ErrNotFound) {
+					log.Error().Msg("no GPG key registered; run 'sekeve server init' first")
+				} else {
+					log.Error().Err(err).Msg("failed to load auth key")
+				}
+				os.Exit(1)
+			}
+
+			authManager := grpcadapter.NewAuthManager(pubKey)
+			server := grpcadapter.NewServer(ctx, store, authManager)
+
+			log.Info().Str("addr", addr).Msg("starting gRPC server")
+			if err := server.Serve(ctx, addr); err != nil {
+				log.Error().Err(err).Msg("server error")
+				os.Exit(1)
+			}
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVar(&addr, "addr", ":50051", "Address to listen on")
+	cmd.Flags().StringVar(&dataPath, "data", "./sekeve.db", "Path to bbolt database")
+	return cmd
+}
